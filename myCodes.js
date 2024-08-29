8f0c02ffd1d4bf2cfd30a2bf7901e714a79545f0
satoshi-maekawa <103913500+satoshi-maekawa@users.noreply.github.com>
2024-08-29 12:15:04 +0900

    Create myCodes.js

---
+++ myCodes.js
@@ -0,0 +1,15 @@
+function calc1(num1, num2) {
+  document.getElementById('result').value = Number(num1) + Number(num2);
+}
+
+function calc2(num1, num2) {
+  document.getElementById('result').value = Number(num1) - Number(num2);
+}
+
+function calc3(num1, num2) {
+  document.getElementById('result').value = Number(num1) * Number(num2);
+}
+
+function calc4(num1, num2) {
+  document.getElementById('result').value = Number(num1) / Number(num2);
+}
